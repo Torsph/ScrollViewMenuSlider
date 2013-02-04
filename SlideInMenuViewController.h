6de9394f614f4d4a80fe396bb71fb6c0c96da9c0
alelangs <alexander.langstrand@finn.no>
2013-02-04 17:09:01 +0100

    Removed unneeded lines.

--- SlideInMenuViewController.h
+++ SlideInMenuViewController.h
@@ -7,31 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
-
 typedef enum {
     Upwards = -1,
     None = 0,
     Downwards = 1
 } ScrollDirection;
 
-@interface SlideInMenuViewController : UIViewController<UIScrollViewDelegate>
-
-@property(assign, nonatomic) CGFloat lastContentOffset;
-
+@interface SlideInMenuViewController : UIViewController
 -(id)initWithMenuView:(UIView *)menu;
--(void)menuPushed:(id)sender;
-
-// Menu methods
--(int)getDirection:(UIScrollView *)scrollView;
--(BOOL)menuViewIsWithinBounds:(CGRect)cgRect withPoint:(CGPoint)cgPoint;
--(void)lockMenuViewInPlace:(UIScrollView *)scrollView;
--(void)slideMenuView:(UIView *)menuView toYPos:(CGFloat)cgFloat;
-
-// ScrollViewDelegateMethods
--(void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView;
--(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView;
--(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate;
 
 @end
 
